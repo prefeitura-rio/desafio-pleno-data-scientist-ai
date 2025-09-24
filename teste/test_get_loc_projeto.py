6c5d545527e0bb6615bda54567a1251dcecc4bf9
Thiago Barral <56553065+thiagobarral@users.noreply.github.com>
2025-09-24 14:47:08 -0300

    integração do agente Hugging Face no main_hface e ajustes para uso do token

---
+++ teste/test_get_loc_projeto.py
@@ -0,0 +1,6 @@
+from google.cloud import bigquery
+from dotenv import load_dotenv
+load_dotenv()
+client = bigquery.Client()
+dataset = client.get_dataset("datario.dados_mestres")
+print(dataset.location)
